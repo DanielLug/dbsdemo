eb4312ae4bb9c5afc99c5522e5e77d6dd6c9fd9e
DanielLug <32488350+DanielLug@users.noreply.github.com>
2017-10-04 09:54:04 +0100

    Create moviesapi.php

---
+++ moviesapi.php
@@ -0,0 +1,25 @@
+<?php
+
+class Movie{
+  public $director;
+  public $movieTitle;
+  public $rating;
+  }
+
+
+$movies = array();
+
+for ($i =0; $i < 10; $i++){
+  $movie = new Movie();
+  $movie->title = "title $i";
+  $movie -> rating = "rating $i";
+  $movie -> director ="director $i";
+  
+  $movies[] = $movie;
+}
+/* Set the header tzpe for output */
+header ('Content-Type: application/json');
+/* Create a variable to hold zour JSON data*/
+$jsonOutput = json_encode($items);
+/* Output the JSON data */
+echo $jsonOutput;
